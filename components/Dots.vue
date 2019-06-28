b041a4c1dd6521326ed588daa76a6da5aac948ce
lichingchester <chester.li@onepage.com.hk>
2019-06-28 12:38:47 +0800

    init

---
+++ components/Dots.vue
@@ -0,0 +1,66 @@
+<template>
+  <svg
+    ref="svg"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlns:xlink="http://www.w3.org/1999/xlink"
+  ></svg>
+</template>
+
+<script>
+export default {
+  mounted() {
+    // initial
+    const dotSpacing = 20;
+
+    console.log(dotSpacing);
+
+    // const namespace = 'http://www.w3.org/2000/svg';
+
+    // let x = 0;
+    // let y = 0;
+    // let isKeepDraw = true;
+    // let isEndWidth = false;
+    // let isEndHeight = false;
+
+    // while (isKeepDraw) {
+    //   while (!isEndWidth) {
+    //     console.log('x', x, 'y', y);
+
+    //     const circle = document.createElementNS(namespace, 'circle');
+    //     circle.setAttribute('cx', x);
+    //     circle.setAttribute('cy', y);
+    //     circle.setAttribute('r', 5);
+    //     this.$refs.svg.appendChild(circle);
+
+    //     x += dotSpacing;
+
+    //     if (x >= 1920) {
+    //       isEndWidth = true;
+    //     }
+    //   }
+    //   x = 0;
+    //   y += dotSpacing;
+
+    //   if (y >= 920) {
+    //     isEndHeight = true;
+    //   } else {
+    //     isEndWidth = false;
+    //   }
+
+    //   if (isEndHeight && isEndWidth) {
+    //     isKeepDraw = false;
+    //   }
+    // }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+svg {
+  position: absolute;
+  width: 1920px;
+  height: 100%;
+  left: 50%;
+  transform: translateX(-50%);
+}
+</style>
---
+++ components/SVGDemo.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="background">
+    <svg ref="svg" xmlns="http://www.w3.org/2000/svg">
+      <circle cx="200" cy="200" r="20" fill="blue" class="dot" />
+    </svg>
+  </div>
+</template>
+
+<script>
+import { TweenMax, Power4 } from 'gsap';
+
+export default {
+  mounted() {
+    const svg = this.$refs.svg;
+
+    TweenMax.to(svg.querySelector('.dot'), 1.5, {
+      scale: 0,
+      transformOrigin: 'center',
+      repeat: 2,
+      yoyo: true,
+      ease: Power4.easeInOut
+    });
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+svg {
+  width: 100%;
+  height: 100%;
+}
+
+.background {
+  position: fixed;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>
